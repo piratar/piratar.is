1bbbb21e4becf4dea1bc0ba4294e7135e788a2e6
Viktor Smari <viktorsmari@gmail.com>
2016-08-17 20:52:08 +0200

    Replace tabs with spaces and reindent

--- wp-content/themes/piratar/templates/bokhald_template.php
+++ wp-content/themes/piratar/templates/bokhald_template.php
@@ -1,23 +1,18 @@
-﻿<?php 
-    /*
-	Template Name: Bókhald
-	*/
-	
-get_header(); ?>
+﻿<?php
+    /* Template Name: Bókhald */
+    get_header();
+?>
 
 <div class="efnid">
-	<div class="wrapper">
-		<div class="alpha full">
-			<?php while ( have_posts() ) : the_post(); ?>
-			
-            <h2 class="section-title"><?php echo the_title(); ?></h2>
-            <div class="splitter h20"></div>
-			
-			
-            <?php the_content(); ?>
-			<?php get_template_part( 'content', "bokhald" ); ?>
-			<?php endwhile; ?>
-		</div>
-	</div>
+    <div class="wrapper">
+        <div class="alpha full">
+            <?php while ( have_posts() ) : the_post(); ?>
+                <h2 class="section-title"><?php echo the_title(); ?></h2>
+                <div class="splitter h20"></div>
+                <?php the_content(); ?>
+                <?php get_template_part( 'content', "bokhald" ); ?>
+            <?php endwhile; ?>
+        </div>
+    </div>
 </div>
 <?php get_footer(); ?>
