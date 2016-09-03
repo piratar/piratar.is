5287b3f8f80f38c3d267cfcd58e19cb3fbec6663
Viktor Smari <viktorsmari@gmail.com>
2016-09-03 16:04:22 +0200

    Althingiskosningar: Position boxes

--- wp-content/themes/piratar-child/templates/althingiskosningar.php
+++ wp-content/themes/piratar-child/templates/althingiskosningar.php
@@ -8,8 +8,8 @@ get_header(); ?>
 		<h3>Stefnan</h3>
 	</div>
 
-	<h3>Topp 5 úr kjördæmum</h3>
 	<div id="boxin_kynning">
+		<h3>Topp 5 úr kjördæmum</h3>
 		<div class="wrapper">
 			<div class="box">
 				<?php the_field('nordvesturbox', 42); ?>
@@ -18,7 +18,7 @@ get_header(); ?>
 				<?php the_field('sudurbox', 42); ?>
 			</div>
 			<div id="myList" class="box">
-				<?php the_field('capitalbox', 42); ?>            
+				<?php the_field('capitalbox', 42); ?>
 			</div>
 		</div>
 		<div class="wrapper">
@@ -29,24 +29,37 @@ get_header(); ?>
 				<?php the_field('forsidubox_5', 42); ?>
 			</div>
 			<div id="myList" class="box">
-				<?php the_field('forsidubox_6', 42); ?>            
+				<?php the_field('forsidubox_6', 42); ?>
 			</div>
 		</div>
+		<h3>Sja alla</h3>
 	</div>
 
-	<h3>Píratar í fréttum</h3>
 	<div id="boxin_kynning" class="">
+		<h3>Píratar í fréttum</h3>
 		<div class="wrapper">
 			<div class="alpha full">
 				<?php the_content(); ?>
 			</div>
 			<div class="box nedrabox">
-				<?php the_field('kosningarbox1', 42); ?>            
+				<?php the_field('kosningarbox1', 42); ?>
 			</div>
 		</div>
 	</div>
 
 
-	<h3>Hvernig á að kjósa?</h3>
+	<div id="boxin_kynning" class="">
+		<div class="wrapper">
+			<div class="box">
+				<h3>Hvernig á að kjósa?</h3>
+			</div>
+			<div class="box">
+				<h3>Fyrir fjölmiðla</h3>
+			</div>
+			<div class="box">
+				<h3>Taka þátt</h3>
+			</div>
+		</div>
+	</div>
 
 <?php get_footer(); ?>
