7d64f29851e4ccc66490f441ab5d0ad4f87fd47c
Viktor Smari <viktorsmari@gmail.com>
2016-09-03 09:36:47 +0200

    Duplicate h2 entry

--- wp-content/themes/piratar/single-frambjodendur.php
+++ wp-content/themes/piratar/single-frambjodendur.php
@@ -4,7 +4,6 @@
     <div class="wrapper">
         <div class="alpha full">
             <?php while ( have_posts() ) : the_post(); ?>
-                <h2 class="section-title"></h2>
                 <h2 class="section-title">Frambjóðandi</h2>
                 <div class="hr hr-short hr-center avia-builder-el-11 el_after_av_textblock el_before_av_textblock ">
                     <span class="hr-inner ">
