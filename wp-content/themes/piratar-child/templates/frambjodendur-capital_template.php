259db2f0e508a8b6b52a9132456f6eaa7a8307af
Viktor Smari <viktorsmari@gmail.com>
2016-09-04 12:05:50 +0200

    Border around each candidate on frambjodendur page

--- wp-content/themes/piratar-child/templates/frambjodendur-capital_template.php
+++ wp-content/themes/piratar-child/templates/frambjodendur-capital_template.php
@@ -21,26 +21,28 @@
                 $terms = wp_get_post_terms($post->ID, $taxonomies,  array("fields" => "names"));
         ?>
 
-            <div class="col-md-4 allborder h500" id="<?php echo $fid ?>" >
-                <div class="row">
-                    <div class="col-md-4">
-                        <a href="<?php echo esc_url(get_permalink()); ?>"><?php if(the_post_thumbnail('thingfolk_thumb', array( 'class' => 'img-responsive'))) ?></a>
-                    </div>
-                    <div class="col-md-8">
-                        <h3>
-                            <a href="<?php echo esc_url(get_permalink()); ?>" ><?php the_title(); ?></a>
-                        </h3>
-                        <p style="padding:5px;">
-                            <?php echo excerpt(90); ?>
-                        </p>
-                        <h4><a href="<?php the_permalink(); ?>">Kynning á frambjóðenda</a></h4>
-                        <?php
-                            $x_hlekkur = get_field( "kosnignarvefur" );
+            <div class="col-md-4 h500" id="<?php echo $fid ?>" >
+                <div class="allborder">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <a href="<?php echo esc_url(get_permalink()); ?>"><?php if(the_post_thumbnail('thingfolk_thumb', array( 'class' => 'img-responsive'))) ?></a>
+                        </div>
+                        <div class="col-md-8">
+                            <h3>
+                                <a href="<?php echo esc_url(get_permalink()); ?>" ><?php the_title(); ?></a>
+                            </h3>
+                            <p style="padding:5px;">
+                                <?php echo excerpt(90); ?>
+                            </p>
+                            <h4><a href="<?php the_permalink(); ?>">Kynning á frambjóðenda</a></h4>
+                            <?php
+                                $x_hlekkur = get_field( "kosnignarvefur" );
 
-                            if ($x_hlekkur) {
-                                echo '<h4><a href="'. $x_hlekkur .'">Upplýsingar um frambjóðanda á kosningavef Pírata</a></h4>';
-                            }
-                        ?>
+                                if ($x_hlekkur) {
+                                    echo '<h4><a href="'. $x_hlekkur .'">Upplýsingar um frambjóðanda á kosningavef Pírata</a></h4>';
+                                }
+                            ?>
+                        </div>
                     </div>
                 </div>
             </div>
