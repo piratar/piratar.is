3159d6fdb70ee288cb21bcdd7fdcfdd43a6b9342
Viktor Smari <viktorsmari@gmail.com>
2016-09-04 13:37:59 +0200

    Purple boxes height

--- wp-content/themes/piratar-child/templates/althingiskosningar.php
+++ wp-content/themes/piratar-child/templates/althingiskosningar.php
@@ -17,32 +17,32 @@ get_header(); ?>
         <h3>Topp 5 úr kjördæmum</h3>
         <div class="row">
             <div class="col-md-6 col-lg-4">
-                <div class="bgpurple">
+                <div class="bgpurple h300">
                     <?php the_field('nordvesturbox', 42); ?>
                 </div>
             </div>
             <div class="col-md-6 col-lg-4">
-                <div class="bgpurple">
+                <div class="bgpurple h300">
                     <?php the_field('sudurbox', 42); ?>
                 </div>
             </div>
             <div class="col-md-6 col-lg-4">
-                <div class="bgpurple">
+                <div class="bgpurple h300">
                     <?php the_field('capitalbox', 42); ?>
                 </div>
             </div>
             <div class="col-md-6 col-lg-4">
-                <div class="bgpurple">
+                <div class="bgpurple h300">
                     <?php the_field('forsidubox_4', 42); ?>
                 </div>
             </div>
             <div class="col-md-6 col-lg-4">
-                <div class="bgpurple">
+                <div class="bgpurple h300">
                     <?php the_field('forsidubox_5', 42); ?>
                 </div>
             </div>
             <div class="col-md-6 col-lg-4">
-                <div class="bgpurple">
+                <div class="bgpurple h300">
                     <?php the_field('forsidubox_6', 42); ?>
                 </div>
             </div>
@@ -64,24 +64,24 @@ get_header(); ?>
             <div class="col-md-6">
                 <div class="row">
                     <div class="col-md-6">
-                        <div class="bgpurple">
+                        <div class="bgpurple h250">
                             aa
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <div class="bgpurple">
+                        <div class="bgpurple h250">
                             bb
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6">
-                        <div class="bgpurple">
+                        <div class="bgpurple h250">
                             cc
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <div class="bgpurple">
+                        <div class="bgpurple h250">
                             dd
                         </div>
                     </div>
@@ -91,17 +91,17 @@ get_header(); ?>
 
         <div class="row">
             <div class="col-md-4">
-                <div class="bgpurple text-center">
+                <div class="bgpurple h300 text-center">
                     <h2 class="white mb30">Hvernig á að kjósa?</h2>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="bgpurple text-center">
+                <div class="bgpurple h300 text-center">
                     <h2 class="white">Fyrir fjölmiðla</h2>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="bgpurple text-center">
+                <div class="bgpurple h300 text-center">
                     <h2 class="white mb30">Taka þátt</h2>
                     <button type="submit" class="btn btn-primary">Smella</button>
                 </div>
