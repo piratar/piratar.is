6456c1f02aa179640da4df5f6f822641d48566d4
Viktor Smari <viktorsmari@gmail.com>
2016-09-04 13:21:25 +0200

    Add partials for all frambjodendur areas

--- wp-content/themes/piratar-child/templates/frambjodendur-capital_template.php
+++ wp-content/themes/piratar-child/templates/frambjodendur-capital_template.php
@@ -1,11 +1,11 @@
-﻿<?php 
+﻿<?php
     /*
     Template Name: Capital Prófkjör
     */
-    
+
     get_header();
  ?>
- 
+
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
@@ -18,7 +18,7 @@
             $custom_query = new WP_Query("kjordaemi=hofudborgarkjordaemi&post_type=frambjodendur&posts_per_page=-1&orderby=rand&order=ASC");
             while($custom_query->have_posts()) : $custom_query->the_post();
             $taxonomies=get_taxonomies('','names');
-                $terms = wp_get_post_terms($post->ID, $taxonomies,  array("fields" => "names"));
+            $terms = wp_get_post_terms($post->ID, $taxonomies,  array("fields" => "names"));
         ?>
 
         <?php get_template_part( 'each', 'candidate'); ?>
--- wp-content/themes/piratar-child/templates/frambjodendur.php
+++ wp-content/themes/piratar-child/templates/frambjodendur.php
@@ -1,50 +1,30 @@
 ﻿<?php 
     /*
-	Template Name: Frambjóðendalisti
-	*/
-	
-	get_header();
+    Template Name: Frambjóðendalisti
+    */
+
+    get_header();
  ?>
- 
-<div class="efnid">  
-    <div class="wrapper">
-		<h2 class="section-title">Frambjóðendur Pírata</h2>
-			<div class="splitter h20"></div>
-				<div class="hr hr-short hr-center avia-builder-el-11 el_after_av_textblock el_before_av_textblock "><span class="hr-inner "><span class="hr-inner-style"></span></span></div>
 
-				<?php
-					$custom_query = new WP_Query("post_type=frambjodendur&posts_per_page=-1&orderby=name&order=ASC"); 
-					
-					while($custom_query->have_posts()) : $custom_query->the_post(); 
-				?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <h2 class="section-title">Frambjóðendur Pírata</h2>
+        </div>
+    </div>
 
-					<div id="<?php echo $fid ?>" class="frambj_listi">
-						<div class="narrow2">
-							<a href="<?php echo esc_url(get_permalink()); ?>"><?php if(the_post_thumbnail('thingfolk_thumb', array( 'class' => 'frambj_thumb_img'))) ?></a>
-						</div>
-						
-						<div class="wide2">
-							<h3>
-								<a href="<?php echo esc_url(get_permalink()); ?>" ><?php the_title(); ?></a>
-							</h3>
+    <div class="row">
+        <?php
+            $custom_query = new WP_Query("post_type=frambjodendur&posts_per_page=-1&orderby=name&order=ASC"); 
+            while($custom_query->have_posts()) : $custom_query->the_post();
+        ?>
 
-							<p>
-								<?php echo excerpt(125); ?>
-							</p>
-							<h4><a href="<?php the_permalink(); ?>">Kynning á frambjóðenda</a></h4>
-							<?php
-								$x_hlekkur = get_field( "kosnignarvefur" );
 
-								if ($x_hlekkur) {
-									echo '<h4><a href="'. $x_hlekkur .'">Upplýsingar um frambjóðanda á kosningavef Pírata</a></h4>';
-								}	
-								
-							?>
-						</div>
-					</div>
 
-				<?php endwhile; ?>
-	</div>
+        <?php get_template_part( 'each', 'candidate'); ?>
+
+        <?php endwhile; ?>
+    </div>
 </div>
 
  <?php get_footer(); ?>
