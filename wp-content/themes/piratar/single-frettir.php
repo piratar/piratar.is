8090b2e5cf93222287f9e48acfa87bae549c0dcf
Andri Sigurðsson <andrisig@gmail.com>
2016-09-23 15:23:23 +0000

    Test 4

--- wp-content/themes/piratar/single-frettir.php
+++ wp-content/themes/piratar/single-frettir.php
@@ -1,23 +1,27 @@
 <?php get_header(); ?>
 
-<?php if (has_post_thumbnail( $post->ID ) ): ?>
 <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) ); ?>
-    <div id="imagebanner">
-        <article>
-            <figure style="background-image:url(<?php echo $image[0]; ?>);"></figure>
-            <div class="coloroverlay"></div>
-            <div class="wrapper">
-                <div class="tourinfo">
-                    <h1 class="h1_title"><?php the_title(); ?></h1>
-                    <div class="readmoretakkar">
-                        <!-- Þurfum við þennan takka? (AVJ) --> 
-                        <span class="title_time"><?php the_date('d.m.Y'); ?></span>
-                    </div>
-                </div>
+
+<div class="section section-card section-title <?php if($image) echo " section-bg-image" ?>" style="background-image: url(<?php echo $image[0] ?>);">
+
+    <div class="section-overlay"></div>
+
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-sm-12">
+
+                <h2 class="the-title"><?php the_title(); ?></h2>
+                 <span class="post-date"><?php the_date('d.m.Y'); ?></span>        
+
             </div>
-        </article>
+
+        </div>
+
     </div>
-<?php endif; ?>
+
+</div>
 
 <div class="section section-content">
 
@@ -28,13 +32,13 @@
             <div class="col-sm-9 push-sm-1">
 
                 <?php while ( have_posts() ) : the_post(); ?>
-            <?php if(!has_post_thumbnail( $post->ID ) ): ?>
-                <h1 class=""><?php the_title(); ?></h1>
-                <span class=""><i><?php the_date('d.m.Y'); ?></i></span>
-            <?php endif; ?>
-            <?php //the_breadcrumb();  ?>
-            <?php the_content(); ?>
-        <?php endwhile; ?>         
+                    <?php if(!has_post_thumbnail( $post->ID ) ): ?>
+                        <h1 class=""><?php the_title(); ?></h1>
+                        <span class=""><i><?php the_date('d.m.Y'); ?></i></span>
+                    <?php endif; ?>
+                    <?php //the_breadcrumb();  ?>
+                    <?php the_content(); ?>
+                <?php endwhile; ?>         
 
             </div>
 
